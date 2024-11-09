c2c38517523c62b25e49a1686fb44922ee7694ca
Mark Wade <wade.mark.a@gmail.com>
2024-11-09 16:31:18 -0700

    Added index.ejs and added code to launch server, link to index.ejs with get

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// Setting up Server //
+const app = express();
+const port = 3000; 
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`)
+});
+
+
+
+//Opening website//
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
