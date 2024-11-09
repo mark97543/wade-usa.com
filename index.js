ebf545593fbcd8a1bd0400bc17b15fe0756b6b35
Mark Wade <wade.mark.a@gmail.com>
2024-11-09 16:52:44 -0700

    Added Header and Bootstrap Formatting

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000; 
 
+app.use(express.static('public'));
+
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`)
 });
