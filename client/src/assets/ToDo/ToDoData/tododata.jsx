0e8e9b0a97f80c9fec512ebe5c141cd5fa6dcd82
Mark Wade <wade.mark.a@gmail.com>
2024-12-26 12:41:50 -0700

    Debugging Todo 1

--- client/src/assets/ToDo/ToDoData/tododata.jsx
+++ client/src/assets/ToDo/ToDoData/tododata.jsx
@@ -5,24 +5,52 @@ import './tododata.css'
 import { ToDoContext } from '../todo';
 
 const ToDoData = () => {
-  const {todos}=useContext(ToDoContext)
+    const {todos, setToDos}=useContext(ToDoContext)
 
+    if (!todos) {
+        return <div>Loading...</div>; // Display loading message while data is fetched
+    }
 
-  return (
-    <div id='tododisplay'>
-        <input className="form-check-input todocheckbox" type="checkbox" value="" ></input>
-        <div id='todoeffect'> 
-            <input type="text" className="form-control tododatainput" placeholder="Default input"></input>
+    if (todos.length === 0) {
+        return <div>No todos yet</div>
+    }
+
+    const handleCheckboxChange = (id) => {
+        setToDos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    console.log(todos)
+    return (
+        <div>
+            <ul>
+                {todos.map((todo, index)=>(
+                    <li id='tododisplay' key={todo.id}>
+                    <input
+                        className="form-check-input todocheckbox"
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => handleCheckboxChange(todo.id)}
+                    />
+                    <div id='todoeffect'>
+                        <input
+                            type="text"
+                            className="form-control tododatainput"
+                            placeholder="Default input"
+                            defaultValue={todo.item} 
+                        />
+                    </div>
+                    <button type="button" className="btn btn-info" id='tododeletebutton'>
+                        ❌
+                    </button>
+                </li>
+                ))}
+            </ul>
         </div>
-        <button 
-            type="button" 
-            className="btn btn-info" 
-            id='tododeletebutton'
-            >❌
-        </button>
-
-    </div>
-  )
+    )
 }
 
 export default ToDoData; 
