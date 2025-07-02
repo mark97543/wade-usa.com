61fea4e376eabbe612237e32d6196b7f877ab5dc
Mark Wade <wade.mark.a@gmail.com>
2025-07-01 18:54:21 -0600

    feat: completed travel cards formattign. Next iteration will be pagenation and filtering completed trips.

--- packages/auth/src/index.js
+++ packages/auth/src/index.js
@@ -1,3 +1,4 @@
 export * from './AuthContext.jsx'
 export * from './ProtectedRoute.jsx';
 export * from './travelApi.js'
+export * from './functions/timeFunctions.js'
--- packages/features/travel/src/Travel_Card/Travel_Card.jsx
+++ packages/features/travel/src/Travel_Card/Travel_Card.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Travel_card.css'
+import {formatDirectusDateToYYMMDD} from '@wade-usa/auth'
+
 
 function Travel_Card({ item}) {
 
     if(!item.trip_card){
-        item.trip_card="ab95449e-d70f-486b-94d0-affed6a7bfa1.jpg"
+        item.trip_card="ab95449e-d70f-486b-94d0-affed6a7bfa1"
     }
 
  
   return (
     <div className='travel_card_box'>
-        <img className='trip_card_image' src={`https://wade-usa-assets.sfo3.digitaloceanspaces.com/${item.trip_card}`}/>
-        <h3 className='trip_card_title'>{item.trip_title}</h3>
+        <img className='trip_card_image' src={`https://api.wade-usa.com/assets/${item.trip_card}`}/>
+        <h2 className='trip_card_title'>{item.trip_title}</h2>
+        <h3 className='trip_card_dates'><i>{`From ${item.start_date} to ${item.end_date}`}</i></h3>
+        <p className='trip_card_summary'>{item.trip_summary}</p>
+
+        <p className='trip_card_misc_date'><i>{`Created on: ${formatDirectusDateToYYMMDD(item.date_created)} Last Update: ${formatDirectusDateToYYMMDD(item.date_updated)}`}</i></p>
     </div>
   )
 }
