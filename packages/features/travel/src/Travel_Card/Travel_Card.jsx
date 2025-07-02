8fee9c0273b56cef438e3f3ecd0755fdf7cbb181
Mark Wade <wade.mark.a@gmail.com>
2025-07-01 18:11:05 -0600

    Updared Nginx Config for larger uploads

--- packages/features/travel/src/Travel_Card/Travel_Card.jsx
+++ packages/features/travel/src/Travel_Card/Travel_Card.jsx
@@ -4,13 +4,14 @@ import './Travel_card.css'
 function Travel_Card({ item}) {
 
     if(!item.trip_card){
-        console.log("No Image")
+        item.trip_card="ab95449e-d70f-486b-94d0-affed6a7bfa1.jpg"
     }
 
  
   return (
     <div className='travel_card_box'>
-        <p>Travel Cars</p>
+        <img className='trip_card_image' src={`https://wade-usa-assets.sfo3.digitaloceanspaces.com/${item.trip_card}`}/>
+        <h3 className='trip_card_title'>{item.trip_title}</h3>
     </div>
   )
 }
