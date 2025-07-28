d32be4d1d9caed7635871e82a7069fd3edd031df
Mark Wade <wade.mark.a@gmail.com>
2025-07-27 18:10:57 -0600

    Slug Page: Started to build page.

--- packages/features/travelv2/src/Slug_Page/Slug.jsx
+++ packages/features/travelv2/src/Slug_Page/Slug.jsx
@@ -2,16 +2,48 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchTripsBySlug } from '@wade-usa/auth'
 import './Slug.css'
+import {useAuth} from '@wade-usa/auth'
+import {useNavigate} from 'react-router-dom'
+
 
 function Slug() {
   const { slug } = useParams() // Correctly destructure 'slug' to match the route parameter
+  const navigate = useNavigate()
+  const allowedRoles = ['Administrator','Basic']
+  const {user, isLoggedIn} = useAuth()
+  const [item, setItem] = useState(null)
+  
+  const hanfleEditTrip=()=>{
+    navigate(`/travel/editor-page/${slug}`)
+  }
 
-  //TODO: Need to see if user is logged in and if they are go to the editor page. 
+  useEffect(() => {
+    const fetchTrip = async () => {
+      const trips = await fetchTripsBySlug(slug)
+      if (trips && trips.length > 0) {
+        const trip = trips[0] // Assuming slug is unique, take the first result
+        setItem(trip)
+      } else {
+        console.log(`No trip found with slug: ${slug}`)
+      }
+    }
+    fetchTrip()
+  }, [slug]) // Dependency array ensures this runs only when slug changes
+
+  console.log(item)
+  if (!item) {
+    return <div>Loading...</div>
+  }
+
 
 
   return (
-    <div>
-      <h1>{`Slug: ${slug}`}</h1>
+    <div className='slug-container'>
+      {isLoggedIn && allowedRoles.includes(user?.role?.name) ? <button onClick={hanfleEditTrip}>Edit Trip</button> : ""}
+      <img className='slug_banner_picture' src={`https://api.wade-usa.com/assets/${item.banner_picture}`}/>
+      <h1>{item.trip_title}</h1>
+      <h3><i>{`From ${item.start_date} to ${item.end_date}`}</i></h3>
+      <div className='slug_long_summary' dangerouslySetInnerHTML={{ __html: item.long_summary }} />
     </div>
   )
 }
