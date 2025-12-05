a9e06ecf8463d3d193704e4f5167796255ab0905
Mark Wade <wade.mark.a@gmail.com>
2025-12-05 01:21:12 -0700

    :

--- services/main/src/lib/directus.ts
+++ services/main/src/lib/directus.ts
@@ -13,7 +13,14 @@ interface Schema {
         danger_color: string;
         success_color: string;
     };
+    
+    main_landing_site: {
+        id: string;
+        c_pic: string;
+    }[];
 }
+
+
 
 // Environment Variables
 export const ROLES = {
